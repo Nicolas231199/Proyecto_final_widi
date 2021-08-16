454dcb87d60d72c39ab7f42e2a364f1c32134cad
FlutLab.io <>
2021-08-16 14:29:46 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    theme: ThemeData(
+      primaryColor: Colors.yellow,
+      accentColor: Colors.grey,
+    ),
